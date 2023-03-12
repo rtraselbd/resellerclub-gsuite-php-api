99419d9d81207f6c59a7d8167273816c8bb4bb1d
Md Rasel Islam Rafi <31556372+rtraselbd@users.noreply.github.com>
2023-03-12 21:10:21 +0600

    Update ResellerClub.php

--- src/ResellerClub.php
+++ src/ResellerClub.php
@@ -2,9 +2,9 @@
 
 namespace RT\ResellerClub;
 
-use GuzzleHttp\Client as Guzzle;
 use RT\ResellerClub\APIs\Orders;
 use RT\ResellerClub\APIs\GSuite;
+use RT\ResellerClub\APIs\Customers;
 
 /**
  * Class ResellerClub
@@ -16,10 +16,7 @@ class ResellerClub
     public const API_URL = 'https://httpapi.com/api/';
     public const API_TEST_URL = 'https://test.httpapi.com/api/';
 
-    /**
-     * @var Guzzle
-     */
-    private $guzzle;
+    private $baseURL;
 
     /**
      * List of API classes
@@ -41,37 +38,20 @@ class ResellerClub
      * @param int    $userId
      * @param string $apiKey
      * @param bool   $testMode
-     * @param int    $timeout
-     * @param string $bindIp
      *
      * @return void
      */
     public function __construct(
         $userId,
         $apiKey,
-        $testMode = false,
-        $timeout = 0,
-        $bindIp = '0'
+        $testMode = false
     ) {
         $this->authentication = [
             'auth-userid' => $userId,
             'api-key'     => $apiKey,
         ];
 
-        $guzzleConfig = [
-            'base_uri'        => $testMode ? self::API_TEST_URL : self::API_URL,
-            'defaults'        => ['query' => $this->authentication],
-            'verify'          => false,
-            'connect_timeout' => (float)$timeout,
-            'timeout'         => (float)$timeout,
-        ];
-
-        if (!empty($bindIp)) {
-            $guzzleConfig['curl'] = [CURLOPT_INTERFACE => $bindIp];
-            $guzzleConfig['stream_context'] = ['socket' => ['bindto' => $bindIp]];
-        }
-
-        $this->guzzle = new Guzzle($guzzleConfig);
+        $this->baseURL = $testMode ? self::API_TEST_URL : self::API_URL;
     }
 
     /**
@@ -82,9 +62,9 @@ class ResellerClub
     private function _getAPI($api)
     {
         if (empty($this->apiList[$api])) {
-            $class = 'RT\\ResellerClub\\APIs\\'.$api;
+            $class = 'RT\\ResellerClub\\APIs\\' . $api;
             $this->apiList[$api] = new $class(
-                $this->guzzle,
+                $this->baseURL,
                 $this->authentication
             );
         }
@@ -106,5 +86,12 @@ class ResellerClub
     public function orders()
     {
         return $this->_getAPI('Orders');
+    }
+    /**
+     * @return Customers
+     */
+    public function customers()
+    {
+        return $this->_getAPI('Customers');
     }
 }
